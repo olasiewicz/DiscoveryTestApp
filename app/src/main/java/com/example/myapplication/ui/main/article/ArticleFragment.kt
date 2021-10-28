5e58746af1181414a355aa55754ffce3f561bd2e
wojciech.olasiewicz <wojciech.olasiewicz@intive.com>
2021-10-28 10:34:37 +0200

    article layout

--- app/src/main/java/com/example/myapplication/ui/main/article/ArticleFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/main/article/ArticleFragment.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication.ui.main.article
 
+import android.content.Intent
+import android.content.Intent.ACTION_SEND
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.myapplication.R
@@ -43,12 +46,25 @@ class ArticleFragment : Fragment() {
             .into(binding.imageArticle)
 
         binding.apply {
-//            tvTitle.text = args.title
-//            tvAuthor.text = args.author
-//            tvDate.text = args.date
-//            tvTeaser.text = args.teaser
+            tvTitle.text = args.title
+            tvAuthor.text = args.author
+            tvDate.text = args.date
+            tvTeaser.text = args.teaser
+            imageBack.setOnClickListener{
+               findNavController().navigate(ArticleFragmentDirections.actionArticleFragment2ToHomeFragment())
+            }
+            imageShare.setOnClickListener {
+                val intentShare = Intent(Intent(ACTION_SEND))
+                intentShare.apply {
+                    type = "text/plant"
+                    putExtra(Intent.EXTRA_SUBJECT,"Share")
+                }
+                startActivity(Intent.createChooser(intentShare, "Sharing"))
+            }
         }
     }
+
+
 
     override fun onDestroyView() {
         super.onDestroyView()
